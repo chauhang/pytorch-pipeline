03e7c7211ed7c5cfb1479c97a3e0f7818e4e8db2
Shrinath Suresh <shrinath@ideas2it.com>
2021-05-14 22:46:07 +0530

    Updating cookie variable
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- pytorch_pipeline/examples/cifar10/pipeline.py
+++ pytorch_pipeline/examples/cifar10/pipeline.py
@@ -164,7 +164,7 @@ def pytorch_cifar10(
         pred_op(
             host_name=isvc_name,
             input_request=input_req,
-            cookie=cookie,
+            cookie="authservice_session=${cookie}",
             url=ingress_gateway,
             model=model,
             inference_type="predict",
@@ -176,7 +176,7 @@ def pytorch_cifar10(
         pred_op(
             host_name=isvc_name,
             input_request=input_req,
-            cookie=cookie,
+            cookie="authservice_session=${cookie}",
             url=ingress_gateway,
             model=model,
             inference_type="explain",
