afd0a7e866a1333acc37099944651a0ba31df2c7
Shrinath Suresh <shrinath@ideas2it.com>
2021-05-19 15:55:01 +0530

    Adding confusion matrix for bert
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- pytorch_pipeline/examples/bert/agnews_classification_pytorch.py
+++ pytorch_pipeline/examples/bert/agnews_classification_pytorch.py
@@ -54,6 +54,11 @@ parser.add_argument(
     help="Path to write mlpipeline-metrics.json",
 )
 
+parser.add_argument(
+    "--confusion_matrix_url",
+    type=str,
+    help="Minio url to generate confusion matrix",
+)
 
 parser = pl.Trainer.add_argparse_args(parent_parser=parser)
 
@@ -124,13 +129,40 @@ mar_config = {
 
 MarGeneration(mar_config=mar_config).generate_mar_file(mar_save_path=args["checkpoint_dir"])
 
+classes = [
+    "World",
+    "Sports",
+    "Business",
+    "Sci/Tech",
+]
+
+model = trainer.ptl_trainer.model
+
+target_index_list = list(set(model.target))
+
+class_list = []
+for index in target_index_list:
+    class_list.append(classes[index])
+
+confusion_matrix_dict = {
+    "actuals": model.target,
+    "preds": model.preds,
+    "classes": class_list,
+    "url": args["confusion_matrix_url"],
+}
+
+test_accuracy = round(float(model.test_acc.compute()), 2)
+
+print("Model test accuracy: ", test_accuracy)
+
+
 visualization_arguments = {
     "input": {
         "tensorboard_root": args["tensorboard_root"],
         "checkpoint_dir": args["checkpoint_dir"],
         "dataset_path": args["dataset_path"],
         "model_name": args["model_name"],
-        # "confusion_matrix_url": args["confusion_matrix_url"],
+        "confusion_matrix_url": args["confusion_matrix_url"],
     },
     "output": {
         "mlpipeline_ui_metadata": args["mlpipeline_ui_metadata"],
@@ -143,6 +175,9 @@ markdown_dict = {"storage": "inline", "source": visualization_arguments}
 print("Visualization Arguments: ", markdown_dict)
 
 visualization = Visualization(
+    test_accuracy=test_accuracy,
+    confusion_matrix_dict=confusion_matrix_dict,
     mlpipeline_ui_metadata=args["mlpipeline_ui_metadata"],
+    mlpipeline_metrics=args["mlpipeline_metrics"],
     markdown=markdown_dict,
 )
