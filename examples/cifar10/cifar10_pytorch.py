b3403ca761c641d7af49f1e615f18648640b67e1
ankan94 <ankan@ideas2it.com>
2021-05-28 20:15:14 +0530

    Fixing code changes
    Signed-off-by: ankan94 <ankan@ideas2it.com>

--- examples/cifar10/cifar10_pytorch.py
+++ examples/cifar10/cifar10_pytorch.py
@@ -2,6 +2,7 @@ import pytorch_lightning as pl
 import os
 from pytorch_pipeline.components.trainer.component import Trainer
 from pytorch_pipeline.components.mar.mar_generation import MarGeneration
+from pathlib import Path
 from argparse import ArgumentParser
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import (
@@ -109,7 +110,7 @@ if "profiler" in args and args["profiler"] != "":
 data_module_args = {"train_glob": args["dataset_path"]}
 
 #Setting tensorboard folder
-Path(tensorboard_root).mkdir(parents=True, exist_ok=True)
+Path(args["tensorboard_root"]).mkdir(parents=True, exist_ok=True)
 
 # Initiating the training process
 trainer = Trainer(
@@ -123,19 +124,19 @@ trainer = Trainer(
 
 # Mar file generation
 
-mar_config = {
-    "MODEL_NAME": "cifar10_test",
-    "MODEL_FILE": "examples/cifar10/cifar10_train.py",
-    "HANDLER": "image_classifier",
-    "SERIALIZED_FILE": os.path.join(args["checkpoint_dir"], args["model_name"]),
-    "VERSION": "1",
-    "EXPORT_PATH": args["checkpoint_dir"],
-    "CONFIG_PROPERTIES": "https://kubeflow-dataset.s3.us-east-2.amazonaws.com/config.properties",
-}
+#mar_config = {
+#    "MODEL_NAME": "cifar10_test",
+#    "MODEL_FILE": "examples/cifar10/cifar10_train.py",
+#    "HANDLER": "image_classifier",
+#    "SERIALIZED_FILE": os.path.join(args["checkpoint_dir"], args["model_name"]),
+#    "VERSION": "1",
+#    "EXPORT_PATH": args["checkpoint_dir"],
+#    "CONFIG_PROPERTIES": "https://kubeflow-dataset.s3.us-east-2.amazonaws.com/config.properties",
+#}
 
 
 
-MarGeneration(mar_config=mar_config).generate_mar_file(mar_save_path=args["checkpoint_dir"])
+#MarGeneration(mar_config=mar_config).generate_mar_file(mar_save_path=args["checkpoint_dir"])
 
 classes = [
     "airplane",
