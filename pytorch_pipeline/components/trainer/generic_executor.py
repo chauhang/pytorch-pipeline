ff1eb6da5ab38869c66aa2b7bbec17936181b56d
ankan94 <ankan@ideas2it.com>
2021-05-31 16:59:23 +0530

    Changes in generic executor
    Signed-off-by: ankan94 <ankan@ideas2it.com>

--- pytorch_pipeline/components/trainer/generic_executor.py
+++ pytorch_pipeline/components/trainer/generic_executor.py
@@ -17,7 +17,8 @@ class GenericExecutor(BaseExecutor):
         # TODO: Code to train pretrained model
         pass
 
-    def _GetFnArgs(self, input_dict: dict, output_dict: dict, execution_properties: dict):
+    def _GetFnArgs(self, input_dict: dict, output_dict: dict,
+                   execution_properties: dict):
         """Gets the input/output/execution properties from the dictionary.
 
         Args:
@@ -33,16 +34,20 @@ class GenericExecutor(BaseExecutor):
             module_file_args : A dictionary of model specific args
             data_module_args : A dictionary of data module args.
         """
-        module_file = input_dict.get(standard_component_specs.TRAINER_MODULE_FILE)
-        data_module_file = input_dict.get(standard_component_specs.TRAINER_DATA_MODULE_FILE)
-        trainer_args = execution_properties.get(standard_component_specs.PTL_TRAINER_ARGS)
-        module_file_args = execution_properties.get(standard_component_specs.TRAINER_MODULE_ARGS)
+        module_file = input_dict.get(
+            standard_component_specs.TRAINER_MODULE_FILE)
+        data_module_file = input_dict.get(
+            standard_component_specs.TRAINER_DATA_MODULE_FILE)
+        trainer_args = execution_properties.get(
+            standard_component_specs.PTL_TRAINER_ARGS)
+        module_file_args = execution_properties.get(
+            standard_component_specs.TRAINER_MODULE_ARGS)
         data_module_args = execution_properties.get(
-            (standard_component_specs.TRAINER_DATA_MODULE_ARGS)
-        )
+            (standard_component_specs.TRAINER_DATA_MODULE_ARGS))
         return module_file, data_module_file, trainer_args, module_file_args, data_module_args
 
-    def derive_model_and_data_module_class(self, module_file: str, data_module_file: str):
+    def derive_model_and_data_module_class(self, module_file: str,
+                                           data_module_file: str):
         """Derives the model file and data modul file.
 
         Args :
@@ -63,8 +68,9 @@ class GenericExecutor(BaseExecutor):
         data_module = importlib.import_module(data_module_file.split(".")[0])
 
         for cls in inspect.getmembers(
-            class_module,
-            lambda member: inspect.isclass(member) and member.__module__ == class_module.__name__,
+                class_module,
+                lambda member: inspect.isclass(member) and member.__module__ ==
+                class_module.__name__,
         ):
             model_class = cls[1]
 
@@ -72,12 +78,14 @@ class GenericExecutor(BaseExecutor):
             raise ValueError(f"Unable to load module_file - {module_file}")
 
         for cls in inspect.getmembers(
-            data_module,
-            lambda member: inspect.isclass(member) and member.__module__ == data_module.__name__,
+                data_module,
+                lambda member: inspect.isclass(member) and member.__module__ ==
+                data_module.__name__,
         ):
             data_module_class = cls[1]
 
         if not data_module_class:
-            raise ValueError(f"Unable to load data_module_file - {data_module_file}")
+            raise ValueError(
+                f"Unable to load data_module_file - {data_module_file}")
 
         return model_class, data_module_class
