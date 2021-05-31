d838369d2669fd315bd64bf3fc928f0e8e1b240f
ankan94 <ankan@ideas2it.com>
2021-05-31 19:23:49 +0530

    Adding Docstrings for UT
    Signed-off-by: ankan94 <ankan@ideas2it.com>

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env/python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+"""Unit tests for trainer component."""
 import os
 import shutil
 import sys
@@ -33,7 +40,8 @@ DEFAULT_MODEL_NAME = "model_state_dict.pth"
 DEFAULT_SAVE_PATH = f"/tmp/{DEFAULT_MODEL_NAME}"
 
 
-def invoke_training(trainer_params):
+def invoke_training(trainer_params):  # pylint: disable=W0621
+    """This function invokes the training process."""
     trainer = Trainer(
         module_file=trainer_params["module_file"],
         data_module_file=trainer_params["data_module_file"],
@@ -46,6 +54,11 @@ def invoke_training(trainer_params):
 
 @pytest.mark.parametrize("mandatory_key", MANDATORY_ARGS)
 def test_mandatory_keys_type_check(mandatory_key):
+    """Tests the uncexpected 'type' of mandatory args.
+
+    Args:
+        mandatory_key : mandatory arguments for inivoking training
+    """
     trainer_dict = deepcopy(trainer_params)
     test_input = ["input_path"]
     trainer_dict[mandatory_key] = test_input
@@ -58,6 +71,11 @@ def test_mandatory_keys_type_check(mandatory_key):
 
 @pytest.mark.parametrize("optional_key", OPTIONAL_ARGS)
 def test_optional_keys_type_check(optional_key):
+    """Tests the unexpected 'type' of optional args.
+
+    Args:
+        optional_key: optional arguments for invoking training
+    """
     trainer_dict = deepcopy(trainer_params)
     test_input = "test_input"
     trainer_dict[optional_key] = test_input
@@ -70,6 +88,11 @@ def test_optional_keys_type_check(optional_key):
 
 @pytest.mark.parametrize("input_key", MANDATORY_ARGS + ["module_file_args"])
 def test_mandatory_params(input_key):
+    """Test for empty mandatory arguments.
+
+    Args:
+        input_key: name of the mandatory arg for training
+    """
     trainer_dict = deepcopy(trainer_params)
     trainer_dict[input_key] = None
     expected_exception_msg = (
@@ -80,6 +103,7 @@ def test_mandatory_params(input_key):
 
 
 def test_data_module_args_optional():
+    """Test for empty optional argument : data module args"""
     trainer_dict = deepcopy(trainer_params)
     trainer_dict["data_module_args"] = None
     invoke_training(trainer_params=trainer_dict)
@@ -88,6 +112,7 @@ def test_data_module_args_optional():
 
 
 def test_trainer_args_none():
+    """Test for empty trainer specific arguments."""
     trainer_dict = deepcopy(trainer_params)
     trainer_dict["trainer_args"] = None
     expected_exception_msg = r"trainer_args must be a dict"
@@ -96,14 +121,17 @@ def test_trainer_args_none():
 
 
 def test_training_success():
+    """Test the training success case with all required args."""
     trainer = invoke_training(trainer_params=trainer_params)
     assert os.path.exists(DEFAULT_SAVE_PATH)
     os.remove(DEFAULT_SAVE_PATH)
     assert hasattr(trainer, "ptl_trainer")
-    assert isinstance(trainer.ptl_trainer, pytorch_lightning.trainer.trainer.Trainer)
+    assert isinstance(trainer.ptl_trainer,
+                      pytorch_lightning.trainer.trainer.Trainer)
 
 
 def test_training_success_with_custom_model_name():
+    """Test for successful training with custom model name."""
     tmp_dir = tempfile.mkdtemp()
     custom_model_name_dict = deepcopy(trainer_params)
     custom_model_name_dict["module_file_args"]["checkpoint_dir"] = tmp_dir
@@ -114,12 +142,14 @@ def test_training_success_with_custom_model_name():
 
 
 def test_training_success_with_empty_module_file_args():
+    """Test for successful training with empty module file args."""
     empty_args_dict = deepcopy(trainer_params)
     empty_args_dict["module_file_args"] = {}
     invoke_training(trainer_params=trainer_params)
 
 
 def test_training_success_with_empty_trainer_args():
+    """Test for successful training with empty trainer args."""
     tmp_dir = tempfile.mkdtemp()
     empty_args_dict = deepcopy(trainer_params)
     empty_args_dict["module_file_args"]["max_epochs"] = 5
@@ -131,6 +161,7 @@ def test_training_success_with_empty_trainer_args():
 
 
 def test_training_success_with_empty_data_module_args():
+    """Test for successful training with empty data module args."""
     tmp_dir = tempfile.mkdtemp()
     tmp_trainer_parms = deepcopy(trainer_params)
     tmp_trainer_parms["module_file_args"]["checkpoint_dir"] = tmp_dir
@@ -142,6 +173,7 @@ def test_training_success_with_empty_data_module_args():
 
 
 def test_trainer_output():
+    """Test for successful training with proper saving of training output."""
     tmp_dir = tempfile.mkdtemp()
     tmp_trainer_parms = deepcopy(trainer_params)
     tmp_trainer_parms["module_file_args"]["checkpoint_dir"] = tmp_dir
@@ -149,7 +181,9 @@ def test_trainer_output():
 
     assert hasattr(trainer, "output_dict")
     assert trainer.output_dict is not None
-    assert trainer.output_dict["model_save_path"] == os.path.join(tmp_dir, DEFAULT_MODEL_NAME)
-    assert isinstance(trainer.output_dict["ptl_trainer"], pytorch_lightning.trainer.trainer.Trainer)
+    assert trainer.output_dict["model_save_path"] == os.path.join(
+        tmp_dir, DEFAULT_MODEL_NAME)
+    assert isinstance(trainer.output_dict["ptl_trainer"],
+                      pytorch_lightning.trainer.trainer.Trainer)
     print("*" * 1000)
     print(type(trainer.output_dict["ptl_trainer"]))
