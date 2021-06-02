79aa2acb52eb6d7bad26d56a1ba0abdb7bdb6970
Shrinath Suresh <shrinath@ideas2it.com>
2021-06-03 01:03:32 +0530

    Replacing all variables due to external pip dependency on test
    Signed-off-by: Shrinath Suresh <shrinath@ideas2it.com>

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -2,7 +2,6 @@ import pytest
 import os
 import json
 from pytorch_pipeline.components.visualization.component import Visualization
-from pytorch_pipeline.types import standard_component_specs
 from unittest.mock import patch
 import tempfile
 
@@ -12,26 +11,22 @@ metdata_dir = tempfile.mkdtemp()
 @pytest.fixture(scope="class")
 def viz_params():
     VIZ_PARAMS = {
-        standard_component_specs.VIZ_MLPIPELINE_UI_METADATA: os.path.join(
-            metdata_dir, "mlpipeline_ui_metadata.json"
-        ),
-        standard_component_specs.VIZ_MLPIPELINE_METRICS: os.path.join(
-            metdata_dir, "mlpipeline_metrics"
-        ),
-        standard_component_specs.VIZ_CONFUSION_MATRIX_DICT: {},
-        standard_component_specs.VIZ_TEST_ACCURACY: 99.05,
-        standard_component_specs.VIZ_MARKDOWN: {"source": "dummy_value", "storage": "dummy"},
+        "mlpipeline_ui_metadata": os.path.join(metdata_dir, "mlpipeline_ui_metadata.json"),
+        "mlpipeline_metrics": os.path.join(metdata_dir, "mlpipeline_metrics"),
+        "confusion_matrix_dict": {},
+        "test_accuracy": 99.05,
+        "markdown": {"source": "dummy_value", "storage": "dummy"},
     }
     return VIZ_PARAMS
 
 
 def generate_visualization(viz_params: dict):
     viz_obj = Visualization(
-        mlpipeline_ui_metadata=viz_params[standard_component_specs.VIZ_MLPIPELINE_UI_METADATA],
-        mlpipeline_metrics=viz_params[standard_component_specs.VIZ_MLPIPELINE_METRICS],
-        confusion_matrix_dict=viz_params[standard_component_specs.VIZ_CONFUSION_MATRIX_DICT],
-        test_accuracy=viz_params[standard_component_specs.VIZ_TEST_ACCURACY],
-        markdown=viz_params[standard_component_specs.VIZ_MARKDOWN],
+        mlpipeline_ui_metadata=viz_params["mlpipeline_ui_metadata"],
+        mlpipeline_metrics=viz_params["mlpipeline_metrics"],
+        confusion_matrix_dict=viz_params["confusion_matrix_dict"],
+        test_accuracy=viz_params["test_accuracy"],
+        markdown=viz_params["markdown"],
     )
 
     return viz_obj.output_dict
@@ -40,14 +35,14 @@ def generate_visualization(viz_params: dict):
 @pytest.mark.parametrize(
     "viz_key",
     [
-        standard_component_specs.VIZ_CONFUSION_MATRIX_DICT,
-        standard_component_specs.VIZ_TEST_ACCURACY,
-        standard_component_specs.VIZ_MARKDOWN,
+        "confusion_matrix_dict",
+        "test_accuracy",
+        "markdown",
     ],
 )
 def test_invalid_type_viz_params(viz_params, viz_key):
     viz_params[viz_key] = "dummy"
-    if viz_key == standard_component_specs.VIZ_TEST_ACCURACY:
+    if viz_key == "test_accuracy":
         expected_type = "<class 'float'>"
     else:
         expected_type = "<class 'dict'>"
@@ -61,8 +56,8 @@ def test_invalid_type_viz_params(viz_params, viz_key):
 @pytest.mark.parametrize(
     "viz_key",
     [
-        standard_component_specs.VIZ_MLPIPELINE_UI_METADATA,
-        standard_component_specs.VIZ_MLPIPELINE_METRICS,
+        "mlpipeline_ui_metadata",
+        "mlpipeline_metrics",
     ],
 )
 def test_invalid_type_metadata_path(viz_params, viz_key):
@@ -78,15 +73,15 @@ def test_invalid_type_metadata_path(viz_params, viz_key):
 @pytest.mark.parametrize(
     "viz_key",
     [
-        standard_component_specs.VIZ_MLPIPELINE_UI_METADATA,
-        standard_component_specs.VIZ_MLPIPELINE_METRICS,
+        "mlpipeline_ui_metadata",
+        "mlpipeline_metrics",
     ],
 )
 def test_default_metadata_path(viz_params, viz_key):
     viz_params[viz_key] = None
     expected_output = {
-        standard_component_specs.VIZ_MLPIPELINE_UI_METADATA: "/mlpipeline-ui-metadata.json",
-        standard_component_specs.VIZ_MLPIPELINE_METRICS: "/mlpipeline-metrics.json",
+        "mlpipeline_ui_metadata": "/mlpipeline-ui-metadata.json",
+        "mlpipeline_metrics": "/mlpipeline-metrics.json",
     }
     with patch("test_visualization.generate_visualization", return_value=expected_output):
         output_dict = generate_visualization(viz_params)
@@ -96,12 +91,12 @@ def test_default_metadata_path(viz_params, viz_key):
 def test_custom_metadata_path(viz_params, tmpdir):
     metadata_ui_path = os.path.join(str(tmpdir), "mlpipeline_ui_metadata.json")
     metadata_metrics_path = os.path.join(str(tmpdir), "mlpipeline_metrics.json")
-    viz_params[standard_component_specs.VIZ_MLPIPELINE_UI_METADATA] = metadata_ui_path
-    viz_params[standard_component_specs.VIZ_MLPIPELINE_METRICS] = metadata_metrics_path
+    viz_params["mlpipeline_ui_metadata"] = metadata_ui_path
+    viz_params["mlpipeline_metrics"] = metadata_metrics_path
     output_dict = generate_visualization(viz_params)
     assert output_dict is not None
-    assert output_dict[standard_component_specs.VIZ_MLPIPELINE_UI_METADATA] == metadata_ui_path
-    assert output_dict[standard_component_specs.VIZ_MLPIPELINE_METRICS] == metadata_metrics_path
+    assert output_dict["mlpipeline_ui_metadata"] == metadata_ui_path
+    assert output_dict["mlpipeline_metrics"] == metadata_metrics_path
     assert os.path.exists(metadata_ui_path)
     assert os.path.exists(metadata_metrics_path)
 
@@ -120,10 +115,8 @@ def test_setting_all_keys_to_none(viz_params):
 def test_accuracy_metric(viz_params):
     output_dict = generate_visualization(viz_params)
     assert output_dict is not None
-    metadata_metric_file = viz_params[standard_component_specs.VIZ_MLPIPELINE_METRICS]
+    metadata_metric_file = viz_params["mlpipeline_metrics"]
     assert os.path.exists(metadata_metric_file)
     with open(metadata_metric_file) as fp:
         data = json.load(fp)
-    assert (
-        data["metrics"][0]["numberValue"] == viz_params[standard_component_specs.VIZ_TEST_ACCURACY]
-    )
+    assert data["metrics"][0]["numberValue"] == viz_params["test_accuracy"]
