1511c49b4bd8cd6a8627b0fdc111a60f77c88195
ankan94 <ankan@ideas2it.com>
2021-06-07 13:54:34 +0530

    Resolving pylint and UT changes.
    Signed-off-by: ankan94 <ankan@ideas2it.com>

--- tests/test_minio.py
+++ tests/test_minio.py
@@ -1,29 +1,21 @@
+#!/usr/bin/env/python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# from unittest.mock import patch
-# from unittest import mock
-# from pytorch_kfp_components.components.minio.component import MinIO
-# from pytorch_kfp_components.components.minio.executor import Executor
-# from minio import Minio
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-# def test_minio_upload():
-#     # client = Minio("localhost:9000")
-#     # client.fput_object(bucket_name="test", object_name="image.png", file_path="/home/ubuntu/Documents/facebook/phase2/kubeflow/pytorch-pipeline/image.png")
-#     with mock.patch.object(Executor, "_initiate_minio_client") as client:
-#         # client.side_effect = ValueError
-#         # mock.patch.object(Executor, "_use_default_config")
-#         # mock.patch("Minio.client")
-#         # client = mock_client.return_value
-#         # client.fput_object(bucket_name="test", object_name="image.png", file_path="/home/ubuntu/Documents/facebook/phase2/kubeflow/pytorch-pipeline/image.png")
-#         print("******************** \n\n\n " , dir(client))
-#         MinIO(bucket_name="test", destination="test1", source="/home/ubuntu/Documents/facebook/phase2/kubeflow/pytorch-pipeline/image.png", endpoint="localhost:9000")
-#         print(client.assert_called_once())
-#         print(client.mock_calls)
-
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit Tests for Minio Component."""
+import tempfile
+import os
 import mock
 import pytest
-import tempfile
-import urllib3
-import os
 from pytorch_kfp_components.components.minio.component import MinIO
 from pytorch_kfp_components.components.minio.executor import Executor
 
@@ -32,44 +24,61 @@ tmpdir = tempfile.mkdtemp()
 with open(os.path.join(str(tmpdir), "dummy.txt"), "w") as fp:
     fp.write("dummy")
 
+
 @pytest.fixture(scope="class")
 def minio_inputs():
-    minio_inputs = {
+    """Sets the minio inputs.
+
+    Returns:
+          minio_inputs : dict of inputs for minio uploads.
+    """
+    minio_input = {
         "bucket_name": "dummy",
         "source": f"{tmpdir}/dummy.txt",
         "destination": "dummy.txt",
-        "endpoint": "localhost:9000"
-
+        "endpoint": "localhost:9000",
     }
-    return minio_inputs
+    return minio_input
 
 
-def upload_to_minio(minio_inputs):
-    MinIO(source=minio_inputs["source"], bucket_name=minio_inputs["bucket_name"], destination=minio_inputs["destination"], endpoint=minio_inputs["endpoint"])
+def upload_to_minio(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Uoloads the artifact to minio."""
+    MinIO(
+        source=minio_inputs["source"],
+        bucket_name=minio_inputs["bucket_name"],
+        destination=minio_inputs["destination"],
+        endpoint=minio_inputs["endpoint"],
+    )
 
 
 @pytest.mark.parametrize(
     "key",
     ["source", "bucket_name", "destination", "endpoint"],
 )
-def test_minio_variables_invalid_type(minio_inputs, key):
+def test_minio_variables_invalid_type(minio_inputs, key):  #pylint: disable=redefined-outer-name
+    """Testing for invalid variable types."""
     minio_inputs[key] = ["test"]
-    expected_exception_msg = f"{key} must be of type <class 'str'> but received as {type(minio_inputs[key])}"
+    expected_exception_msg = f"{key} must be of type <class 'str'>" \
+                             f" but received as {type(minio_inputs[key])}"
     with pytest.raises(TypeError, match=expected_exception_msg):
         upload_to_minio(minio_inputs)
 
+
 @pytest.mark.parametrize(
     "key",
     ["source", "bucket_name", "destination", "endpoint"],
 )
-def test_minio_mandatory_param(minio_inputs, key):
+def test_minio_mandatory_param(minio_inputs, key):  #pylint: disable=redefined-outer-name
+    """Testing for invalid minio mandatory parameters."""
     minio_inputs[key] = None
-    expected_exception_msg = f"{key} is not optional. Received value: {minio_inputs[key]}"
+    expected_exception_msg = (
+        f"{key} is not optional. Received value: {minio_inputs[key]}")
     with pytest.raises(ValueError, match=expected_exception_msg):
         upload_to_minio(minio_inputs)
 
 
-def test_missing_access_key(minio_inputs):
+def test_missing_access_key(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Test upload if minio access key is missing."""
     os.environ["MINIO_SECRET_KEY"] = "dummy"
     expected_exception_msg = "Environment variable MINIO_ACCESS_KEY not found"
     with pytest.raises(ValueError, match=expected_exception_msg):
@@ -78,7 +87,8 @@ def test_missing_access_key(minio_inputs):
     os.environ.pop("MINIO_SECRET_KEY")
 
 
-def test_missing_secret_key(minio_inputs):
+def test_missing_secret_key(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Test upload if minio secret key is missing."""
     os.environ["MINIO_ACCESS_KEY"] = "dummy"
     expected_exception_msg = "Environment variable MINIO_SECRET_KEY not found"
     with pytest.raises(ValueError, match=expected_exception_msg):
@@ -86,31 +96,35 @@ def test_missing_secret_key(minio_inputs):
     os.environ.pop("MINIO_ACCESS_KEY")
 
 
-def test_unreachable_endpoint(minio_inputs):
+def test_unreachable_endpoint(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Testing unreachable minio endpoint with invalid minio creds."""
     os.environ["MINIO_ACCESS_KEY"] = "dummy"
     os.environ["MINIO_SECRET_KEY"] = "dummy"
     with pytest.raises(Exception, match="Max retries exceeded with url*"):
         upload_to_minio(minio_inputs)
 
 
-def test_invalid_file_path(minio_inputs):
+def test_invalid_file_path(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Test invalid source file path."""
     minio_inputs["source"] = "dummy"
-    expected_exception_msg = f"Input path - {minio_inputs['source']} does not exists"
+    expected_exception_msg = (
+        f"Input path - {minio_inputs['source']} does not exists")
     with pytest.raises(ValueError, match=expected_exception_msg):
         upload_to_minio(minio_inputs)
 
 
-def test_minio_upload_file(minio_inputs):
+def test_minio_upload_file(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Testing upload of files to minio."""
     with mock.patch.object(Executor, "upload_artifacts_to_minio") as client:
         client.return_value = []
         upload_to_minio(minio_inputs)
         client.asser_called_once()
 
 
-def test_minio_upload_folder(minio_inputs):
+def test_minio_upload_folder(minio_inputs):  #pylint: disable=redefined-outer-name
+    """Testing upload of folder to minio."""
     minio_inputs["source"] = tmpdir
     with mock.patch.object(Executor, "upload_artifacts_to_minio") as client:
         client.return_value = []
         upload_to_minio(minio_inputs)
         client.asser_called_once()
-
